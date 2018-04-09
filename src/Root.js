baf6c32c1a131079f7ebfb8e198e715f79dbcf4a
quanchunlindellpc <quanchunlin108@163.com>
2018-04-09 23:47:55 +0800

    init

---
+++ src/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Root = ({}) => {
+  return (
+    <div className="test-font-color">
+      Shawn`s Demo React
+    </div>
+  );
+}
+
+export default Root;
