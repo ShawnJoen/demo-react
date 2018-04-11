d3028f695e9cf158e414645315fcedfc2246183f
quanchunlindellpc <quanchunlin108@163.com>
2018-04-11 23:47:10 +0800

    sass scss

--- src/Root.js
+++ src/Root.js
@@ -9,6 +9,12 @@ const Root = ({}) => {
       <div className="test-font-color">Shawn`s Demo React</div>
       <button className={cx('test-font-color')}>Test Button</button><br/>
       <div className={cx('test-btn')}>Test Div</div>
+      <a className={cx('test-a-tag')}>Test A Tag</a>
+      <div className="test-border">Test border radius</div><br/>
+      <div className="test-media-div">Test Media div</div><br/>
+      <div className="test-extend-div">Test Extend div</div><br/>
+      <input className="test-input" type="text" placeholder="Test Input"/><br/>
+      <div className="test-function-div">Test Function div</div><br/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'core-js/es6/map';/**IE10或以下版本 ECMA Script6*/
+import 'core-js/es6/set';/**IE10或以下版本 ECMA Script6*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root';
